503451d0e3ea925591edec602aec2135151b3252
Esosa <esosa.agbonavbare@decagon.dev>
2024-05-14 21:00:28 +0100

    Initial Domain and Persistence layer setup

---
+++ FormApplicationCreator.Domain/ApiResponse.cs
@@ -0,0 +1,43 @@
+﻿namespace FormApplicationCreator.Domain
+{
+    public class ApiResponse<T>
+    {
+        public bool IsSuceeded { get; set; }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+        public T Data { get; set; }
+
+        public ApiResponse(bool isSuceeded, int statusCode, string message, List<string> errors, T data)
+        {
+            IsSuceeded = isSuceeded;
+            StatusCode = statusCode;
+            Message = message;
+            Errors = errors;
+            Data = data;
+        }
+
+        public ApiResponse(bool isSuceeded, int statusCode, string message, T data)
+        {
+            IsSuceeded = isSuceeded;
+            StatusCode = statusCode;
+            Message = message;
+            Data = data;
+        }
+
+        public ApiResponse(bool isSuceeded, int statusCode, string message, List<string> errors)
+        {
+            IsSuceeded = isSuceeded;
+            StatusCode = statusCode;
+            Message = message;
+            Errors = errors;
+        }
+
+        public ApiResponse(bool isSuceeded, int statusCode, string message)
+        {
+            IsSuceeded = isSuceeded;
+            StatusCode = statusCode;
+            Message = message;
+        }
+    }
+}
---
+++ FormApplicationCreator.Persistence/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FormApplicationCreator.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormApplicationCreator.Persistence
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<ApplicationForm> ApplicationForms { get; set; }
+        public DbSet<Candidate> Candidates { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Response> Responses { get; set; }
+    }
+}
